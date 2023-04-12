1b26a1b540c47c8da28f2712fe30eb60ab00fab1
Regina <rmyachina@mail.ru>
2023-04-12 21:34:41 +0300

    + mole game

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,50 @@
+let killedHole = document.getElementById("dead");
+let lostedHole = document.getElementById("lost");
+
+let isWin = () => {
+    if (killedHole.textContent >= "10") {
+        killedHole.textContent = 0;
+        alert("You win!");
+    }
+}
+
+let isLost = () => {
+    if (lostedHole.textContent >= "5" ) {
+        lostedHole.textContent = 0
+        return alert("You lose");
+    }
+}
+
+let getHole = (index) => {
+    return document.getElementById(`hole${index}`)
+}
+
+for(let i = 1; i <=9; i++) {
+    getHole(i).onclick = () => {
+        
+        if (getHole(i).classList.contains("hole_has-mole")) {
+            killedHole.textContent++;
+            isWin();
+            
+                
+        } else {
+            lostedHole.textContent++;
+            isLost();
+           
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+
