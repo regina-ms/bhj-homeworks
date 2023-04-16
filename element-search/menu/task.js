b5023c9822f4cc43fa785bb0e40a7c66b3322997
Regina <rmyachina@mail.ru>
2023-04-16 20:29:04 +0300

    add matches in task 3

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -2,14 +2,13 @@ let menuLinkArr = Array.from(document.querySelectorAll(".menu__link"));
 let subMenuArr = Array.from(document.querySelectorAll(".menu_sub"));
 
 menuLinkArr.forEach(item => {
-    //debugger;
     item.onclick = () => {        
         subMenuArr.forEach(item => {
             if(item.classList.contains("menu_active")) {
                 item.classList.remove("menu_active");
             }
         })
-       if(item.nextElementSibling) {
+       if(item.nextElementSibling.matches(".menu_sub")) {
             item.nextElementSibling.classList.add("menu_active");
             return false;
         }
@@ -17,5 +16,5 @@ menuLinkArr.forEach(item => {
 })
 
 
-// не рпазобралась, как в задании использовать closest
+
 
