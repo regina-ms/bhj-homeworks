84e68be352a0dfdf99b269a6bfb3cd5175e25025
Regina <rmyachina@mail.ru>
2023-04-20 21:27:59 +0300

    popup

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,20 +1,17 @@
 let modal = document.querySelector(".modal");
-let modalClose = modal.querySelector(".modal__close");
+let modalClose = Array.from(document.querySelectorAll(".modal__close"));
 let btnShowSucccess = modal.querySelector(".show-success");
 let modalSuccess = document.querySelector("#modal_success");
 let modalSuccessClose = modalSuccess.querySelector(".modal__close");
 
 modal.classList.add("modal_active");
 
-modalClose.onclick = () =>{
-    modal.classList.remove("modal_active")
-}
+modalClose.forEach(item => {
+    item.onclick = () =>{
+        item.closest(".modal_active").classList.remove("modal_active")}
+})
 
 btnShowSucccess.onclick = () => {
     modal.classList.remove("modal_active");
     modalSuccess.classList.add("modal_active");
-}
-
-modalSuccessClose.onclick = () => {
-    modalSuccess.classList.remove("modal_active");
 }
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,80 +1,76 @@
-let sliderArr = Array.from(document.querySelectorAll(".slider__item"));
-let activeSlider = document.querySelector(".slider__item_active");
-let arrowPrev = document.querySelector(".slider__arrow_prev");
-let arrowNext = document.querySelector(".slider__arrow_next");
-let dotArr = Array.from(document.querySelectorAll(".slider__dot"));
-let slideNumber = 0;
 
-let showActiveDot = () => {
-    for(let i = 0 ; i < dotArr.length; i++) {
-        if(dotArr[i].classList.contains("slider__dot_active")) {
-            dotArr[i].classList.remove("slider__dot_active");
-        }
-        if(i === slideNumber) {
-            dotArr[i].classList.add("slider__dot_active");
-        }
-    }
-}
 
-let showNextSlider = () =>{
-    for(let i = 0; i < sliderArr.length; i++) {
-        if(sliderArr[i].classList.contains("slider__item_active")) {
-            sliderArr[i].classList.remove("slider__item_active");
-            activeSlider = sliderArr[i+1];     
-            slideNumber = i+1;
-        }
-        if (!activeSlider) {
-            activeSlider = sliderArr[0];
-            slideNumber = 0;
-        }
-    }
-    activeSlider.classList.add("slider__item_active");  
-}
 
-let showPrevSlider = () =>{
-    for(let i = 0; i < sliderArr.length; i++) {
-        if(sliderArr[i].classList.contains("slider__item_active")) {
-            sliderArr[i].classList.remove("slider__item_active");
-            activeSlider = sliderArr[i-1];
-            slideNumber = i-1;
-        }
-        if (!activeSlider) {
-            activeSlider = sliderArr[sliderArr.length-1];
-            slideNumber = sliderArr.length-1;
-        }
-    }
-    activeSlider.classList.add("slider__item_active");
-}
 
-showActiveDot(); 
 
-arrowPrev.onclick = () => {
-    showPrevSlider();   
-    showActiveDot(); 
-}
 
-arrowNext.onclick = () => {
-    showNextSlider();
-    showActiveDot();
-}
 
-dotArr.forEach((item, index) => {
-    item.onclick = () => {
-        dotArr.forEach(item => {
-            if(item.classList.contains("slider__dot_active")) {
-            item.classList.remove("slider__dot_active")
-        }})
-        item.classList.add("slider__dot_active");
-        slideNumber = index;
-        
-        sliderArr.forEach((item, index) => {
-            if(item.classList.contains("slider__item_active")){
-                item.classList.remove("slider__item_active");
-            }
-            if(index === slideNumber) {
-                item.classList.add("slider__item_active");
-            }
-        })
-    }
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
