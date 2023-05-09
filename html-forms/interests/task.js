1bd914eeed4bc8bd8fbd4c9eb67dc94799bc09ef
Regina <rmyachina@mail.ru>
2023-05-09 16:31:53 +0300

    Доработка

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -6,52 +6,42 @@ let parent;
 let children;
 let checkedChildren =[];
 
-function isChecked(item) {
-    if(item.checked === true) {
-        item.checked = false;
-        return; 
+function toggleChildren(item) {
+    children = Array.from(item.closest(".interest").querySelectorAll('.interests > .interest'));
+    children.forEach(item => 
+        item.querySelector(".interest__check").checked = !item.querySelector(".interest__check").checked
+    )
+}
+
+function updateParent(item) {
+
+    parent = item.closest(".interests_active").closest(".interest");
+
+    children = Array.from(parent.closest(".interest").querySelectorAll('.interests > .interest'));
+    checkedChildren = children.filter(item => item.querySelector(".interest__check").checked === true)
+    if(children.length === checkedChildren.length) {
+        parent.querySelector(".interest__check").indeterminate = false;
+        parent.querySelector(".interest__check").checked = true;
+    } else if(checkedChildren.length === 0) {
+        parent.querySelector(".interest__check").indeterminate = false;
+        parent.querySelector(".interest__check").checked = false;
+    } else {
+        parent.querySelector(".interest__check").indeterminate = true;
     }
 
-    if(item.checked === false) {
-        item.checked = true;
-        return; 
+    if(parent.closest(".interests_active")){
+        updateParent(parent);
     }
 }
 
-
-
-function func(item) {
-    if(item.closest(".interests_active") === null) {
-        children = Array.from(item.closest(".interest").querySelectorAll('.interests > .interest'));
-        for(let item of children) {
-            isChecked(item.querySelector(".interest__check"));
-        }
+document.addEventListener("click", (e) => {
+    if(e.target.closest(".interests_active") === null) {
+        toggleChildren(e.target);
+    } else {
+        toggleChildren(e.target);
+        updateParent(e.target);
     }
+})
 
-    if(item.closest(".interests_active")){
-        parent = item.closest(".interests_active").closest(".interest");
-        children = Array.from(parent.closest(".interest").querySelectorAll('.interests > .interest'));
-        checkedChildren = children.filter(item => item.querySelector(".interest__check").checked === true)
-        if(children.length === checkedChildren.length) {
-            parent.querySelector(".interest__check").indeterminate = false;
-            parent.querySelector(".interest__check").checked = true;
-        } else if(checkedChildren.length === 0) {
-            parent.querySelector(".interest__check").indeterminate = false;
-            parent.querySelector(".interest__check").checked = false;
-        } else {
-            parent.querySelector(".interest__check").indeterminate = true;
-        }
-        
-        if(parent.closest(".interests_active")){
-            func(parent.querySelector(".interest__check"));
-        }
 
-        if(item.closest(".interest").querySelectorAll('.interests > .interest').length!==0){
-            Array.from(item.closest(".interest").querySelectorAll('.interests > .interest')).
-            forEach(item => isChecked(item.querySelector(".interest__check")))       
-        }
-    }
-}
-
-document.addEventListener("click", (e) => func(e.target))
 
